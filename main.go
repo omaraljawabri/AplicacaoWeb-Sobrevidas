73258a5a21bddd730ee0a38833756ab866dabe35
omaraljawabri <164200540+omaraljawabri@users.noreply.github.com>
2024-05-19 19:07:00 -0300

    pegando dados do formulario

--- main.go
+++ main.go
@@ -1,44 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
 )
 
-func login(w http.ResponseWriter, r *http.Request){
-	var fileName = "login.html"
-	t, err := template.ParseFiles(fileName)
+var db = fazConexaoComBanco()
+
+func main() {
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+	http.HandleFunc("/quote", createQuote)
+
+	log.Println("Server rodando na porta 8080")
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil{
-		fmt.Println("Erro ao fazer o parse files", err)
+		log.Fatal(err)
+	}
+}
+
+func fazConexaoComBanco() *sql.DB{
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatalf("Erro ao carregar arquivo .env")
+	}
+
+	usuarioBancoDeDados := os.Getenv("USUARIO")
+	senhaDoUsuario := os.Getenv("SENHA")
+	nomeDoBancoDeDados := os.Getenv("NOME_BANCO_DE_DADOS")
+	dadosParaConexao := "user=" + usuarioBancoDeDados + " dbname=" + nomeDoBancoDeDados + " password=" + senhaDoUsuario + " host=localhost port=5432 sslmode=disable"
+	database, err := sql.Open("postgres", dadosParaConexao)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = database.Query("CREATE TABLE IF NOT EXISTS cadastro(id SERIAL PRIMARY KEY, nome_completo VARCHAR(255) UNIQUE NOT NULL, cpf VARCHAR(15) UNIQUE NOT NULL, cns VARCHAR(15), cbo VARCHAR(15), cnes VARCHAR(15), ine VARCHAR(15), senha VARCHAR(20))")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	return database
+}
+
+func createQuote(w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodPost{
+		http.Redirect(w, r, "/quote", http.StatusSeeOther)
 		return
 	}
-	err = t.ExecuteTemplate(w, fileName, nil)
+	err := r.ParseForm()
 	if err != nil{
-		fmt.Println("Erro ao executar a template", err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-}
+	nomecompleto := r.PostForm.Get("nome_completo")
+	cpf := r.PostForm.Get("cpf")
+	cns := r.PostForm.Get("cns")
+	cbo := r.PostForm.Get("cbo")
+	cnes := r.PostForm.Get("cnes")
+	ine := r.PostForm.Get("ine")
+	senha := r.PostForm.Get("senha")
 
-func loginSubmit(w http.ResponseWriter, r *http.Request){
-
-}
-
-func register(w http.ResponseWriter, r *http.Request){
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request){
-	switch r.URL.Path{
-	case "/login":
-		login(w, r)
-	case "/register":
-	case "/login-submit":
-	default:
+	_, err = db.Exec("INSERT INTO cadastro(nome_completo, cpf, cns, cbo, cnes, ine, senha) VALUES($1, $2, $3, $4, $5, $6, $7)", nomecompleto, cpf, cns, cbo, cnes, ine, senha)
+	if err != nil{
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
-}
-
-func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("", nil)
 }
