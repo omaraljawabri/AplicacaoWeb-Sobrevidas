6acf22367c4fe19f18fad368cea1b17f017d19e7
omaraljawabri <164200540+omaraljawabri@users.noreply.github.com>
2024-05-28 10:04:20 -0300

    att

---
+++ model/pacientes.go
@@ -0,0 +1,22 @@
+package model
+
+type Pacientes struct{
+	ID uint64
+	NomeCompleto string
+	DataNasc string
+	CPF string
+	NomeMae string
+	Sexo string
+	CartaoSUS string
+	Telefone string
+	Email string
+	CEP string
+	Bairro string
+	Rua string
+	Numero string
+	Complemento string
+	Homem string
+	Etilista string
+	Tabagista string
+	LesaoBucal string
+}
